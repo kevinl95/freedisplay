b2843ee9437029e1e890b4d7d7fb65e12254361b
kevinl95 <kevin.loeffler@colorado.edu>
2020-03-21 15:24:06 -0600

    Initial build script.

--- main.py
+++ main.py
@@ -1,33 +1,22 @@
+import cv2
 import numpy as np
 import os
 import pathlib
 import qrcode
+import shutil
 import socket
-from multiprocess import Process, Value
+import tempfile
+import threading
 from flask import Flask, render_template, jsonify
-
-IMAGE_FOLDER = os.path.join("static", "screenimgs")
-app = Flask(__name__)
-app.config["UPLOAD_FOLDER"] = IMAGE_FOLDER
+from mss import mss
+from PIL import Image
 
 
-@app.route("/")
-@app.route("/index")
-def show_index():
-    return render_template("index.html")
-
-
-def capture(app):
-    from mss import mss
-    from PIL import Image
-    import tempfile
-    import shutil
-    import os
-
+def capture(upload_folder):
     sct = mss()
     mon = sct.monitors[1]
-    full_filename = os.path.join(app.config["UPLOAD_FOLDER"], "current.png")
-    redundant_filename = os.path.join(app.config["UPLOAD_FOLDER"], "redundant.png")
+    full_filename = os.path.join(upload_folder, "current.png")
+    redundant_filename = os.path.join(upload_folder, "redundant.png")
     while 1:
         sct_img = sct.grab(mon)
         img = Image.frombytes("RGB", sct_img.size, sct_img.bgra, "raw", "BGRX")
@@ -42,12 +31,21 @@ def capture(app):
 
 
 def open_code():
-    import cv2
-
     cv2.startWindowThread()
     cv2.namedWindow("Scan this code to access your display")
     cv2.imshow("Scan this code to access your display", cv2.imread("displaycode.png"))
     cv2.waitKey(0)
+
+
+IMAGE_FOLDER = os.path.join("static", "screenimgs")
+app = Flask(__name__)
+app.config["UPLOAD_FOLDER"] = IMAGE_FOLDER
+
+
+@app.route("/")
+@app.route("/index")
+def show_index():
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
@@ -69,10 +67,12 @@ if __name__ == "__main__":
     qr.make(fit=True)
     qrimg = qr.make_image(fill_color="black", back_color="white")
     qrimg.save("displaycode.png")
-    cvprocess = Process(target=open_code)
-    captureprocess = Process(target=capture, args=(app,))
+    # cvprocess = Process(target=open_code)
+    cvprocess = threading.Thread(target=open_code, args=())
+    cvprocess.start()  # Start the execution
+    # captureprocess = Process(target=capture, args=(app.config["UPLOAD_FOLDER"],))
+    captureprocess = threading.Thread(
+        target=capture, args=(app.config["UPLOAD_FOLDER"],)
+    )
     captureprocess.start()
-    cvprocess.start()
     app.run(host=localip, use_reloader=False, debug=True)
-    cvprocess.join()
-    captureprocess.join()
--- setup_win.py
+++ setup_win.py
@@ -1,32 +1,30 @@
 import sys
 from cx_Freeze import setup, Executable
 import os.path
+
 PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
-os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tcl8.6')
-os.environ['TK_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tk8.6')
+os.environ["TCL_LIBRARY"] = os.path.join(PYTHON_INSTALL_DIR, "tcl", "tcl8.6")
+os.environ["TK_LIBRARY"] = os.path.join(PYTHON_INSTALL_DIR, "tcl", "tk8.6")
 
 # Dependencies are automatically detected, but it might need fine tuning.
-build_exe_options = {"packages": ["os"], "excludes": [""]}
-
-options = {
-    'build_exe': {
-        'include_files':[
-            os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tk86t.dll'),
-            os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tcl86t.dll'),
-         ],
-    },
+build_exe_options = {
+    "packages": ["os", "pathos", "multiprocess", "multiprocess.context", "jinja2.ext"],
+    "excludes": [""],
+    "include_files": [
+        "static",
+        "templates",
+        os.path.join(PYTHON_INSTALL_DIR, "DLLs", "tk86t.dll"),
+        os.path.join(PYTHON_INSTALL_DIR, "DLLs", "tcl86t.dll"),
+    ],
 }
 
-# GUI applications require a different base on Windows (the default is for a
-# console application).
-base = None
-if sys.platform == "win32":
-    base = "Win32GUI"
+base = "console"
 
-setup(  name = "FreeDisplay",
-        version = "1.0",
-        description = "Share your screen over your local network!" ,
-        author="Kevin Loeffler",
-        options = {"build_exe": build_exe_options,
-                   "options": options},
-        executables = [Executable("main.py", base=base)])
+setup(
+    name="FreeDisplay",
+    version="1.0",
+    description="Share your screen over your local network!",
+    author="Kevin Loeffler",
+    options={"build_exe": build_exe_options},
+    executables=[Executable("main.py", base=base)],
+)
