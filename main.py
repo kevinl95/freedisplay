bac47a74bd5796857a8dd2c469a6aadc7843dc07
kevinl95 <kevin.loeffler@colorado.edu>
2020-03-20 20:39:44 -0600

    Initial working verision.

--- main.py
+++ main.py
@@ -1,16 +1,74 @@
 import numpy as np
 import cv2
+import os
+import pathlib
+import qrcode
+import shutil
+import socket
+import tempfile
+from multiprocessing import Process, Value
 from mss import mss
 from PIL import Image
+from flask import Flask, render_template, jsonify
+
+IMAGE_FOLDER = os.path.join("static", "screenimgs")
+app = Flask(__name__)
+app.config["UPLOAD_FOLDER"] = IMAGE_FOLDER
+
+full_filename = os.path.join(app.config["UPLOAD_FOLDER"], "current.png")
+
+
+@app.route("/")
+@app.route("/index")
+def show_index():
+    return render_template("index.html")
+
 
 sct = mss()
 
 mon = sct.monitors[1]
 
-while 1:
-    sct_img = sct.grab(mon)
-    img = Image.frombytes("RGB", sct_img.size, sct_img.bgra, "raw", "BGRX")
-    cv2.imshow('test', np.array(img))
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-        cv2.destroyAllWindows()
-        break
+
+def capture():
+    while 1:
+        sct_img = sct.grab(mon)
+        img = Image.frombytes("RGB", sct_img.size, sct_img.bgra, "raw", "BGRX")
+        tp = tempfile.NamedTemporaryFile(suffix=".png", delete=False)
+        img.save(tp.name)
+        shutil.copy(tp.name, os.path.join(os.getcwd(), full_filename))
+        tp.close()
+
+
+def open_code():
+    cv2.startWindowThread()
+    cv2.namedWindow("Scan this code to access your display")
+    cv2.imshow("Scan this code to access your display", cv2.imread("displaycode.png"))
+    cv2.waitKey(0)
+
+
+if __name__ == "__main__":
+    pathlib.Path(IMAGE_FOLDER).mkdir(parents=True, exist_ok=True)
+    screenshot = os.path.join(IMAGE_FOLDER, "current.png")
+    if os.path.isfile(screenshot):
+        os.remove(screenshot)
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    localip = s.getsockname()[0]
+    s.close()
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=15,
+        border=4,
+    )
+    qr.add_data("http://" + localip + ":5000")
+    qr.make(fit=True)
+    qrimg = qr.make_image(fill_color="black", back_color="white")
+    qrimg.save("displaycode.png")
+    cvprocess = Process(target=open_code)
+    captureprocess = Process(target=capture)
+    captureprocess.start()
+    cvprocess.start()
+    app.run(host=localip, debug=True, use_reloader=False)
+    cvprocess.join()
+    captureprocess.join()
