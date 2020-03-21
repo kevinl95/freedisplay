9a3216b7e704f7140db903ecf50c6b1d92652393
kevinl95 <kevin.loeffler@colorado.edu>
2020-03-21 14:03:26 -0600

    Replacing multiprocessing code, starting build with cx_Freeze.

--- main.py
+++ main.py
@@ -1,25 +1,14 @@
 import numpy as np
-import cv2
 import os
 import pathlib
 import qrcode
-import shutil
 import socket
-import tempfile
-from multiprocessing import Process, Value
-from mss import mss
-from PIL import Image
+from multiprocess import Process, Value
 from flask import Flask, render_template, jsonify
 
 IMAGE_FOLDER = os.path.join("static", "screenimgs")
 app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = IMAGE_FOLDER
-
-full_filename = os.path.join(app.config["UPLOAD_FOLDER"], "current.png")
-redundant_filename = os.path.join(app.config["UPLOAD_FOLDER"], "redundant.png")
-
-sct = mss()
-mon = sct.monitors[1]
 
 
 @app.route("/")
@@ -28,7 +17,17 @@ def show_index():
     return render_template("index.html")
 
 
-def capture():
+def capture(app):
+    from mss import mss
+    from PIL import Image
+    import tempfile
+    import shutil
+    import os
+
+    sct = mss()
+    mon = sct.monitors[1]
+    full_filename = os.path.join(app.config["UPLOAD_FOLDER"], "current.png")
+    redundant_filename = os.path.join(app.config["UPLOAD_FOLDER"], "redundant.png")
     while 1:
         sct_img = sct.grab(mon)
         img = Image.frombytes("RGB", sct_img.size, sct_img.bgra, "raw", "BGRX")
@@ -43,6 +42,8 @@ def capture():
 
 
 def open_code():
+    import cv2
+
     cv2.startWindowThread()
     cv2.namedWindow("Scan this code to access your display")
     cv2.imshow("Scan this code to access your display", cv2.imread("displaycode.png"))
@@ -69,9 +70,9 @@ if __name__ == "__main__":
     qrimg = qr.make_image(fill_color="black", back_color="white")
     qrimg.save("displaycode.png")
     cvprocess = Process(target=open_code)
-    captureprocess = Process(target=capture)
+    captureprocess = Process(target=capture, args=(app,))
     captureprocess.start()
     cvprocess.start()
-    app.run(host=localip, use_reloader=False)
+    app.run(host=localip, use_reloader=False, debug=True)
     cvprocess.join()
     captureprocess.join()
